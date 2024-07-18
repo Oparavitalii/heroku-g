9df7e972aaa464314e9493d81adcc12a2f0345e5
Vitalii <58904253+Oparavitalii@users.noreply.github.com>
2024-07-18 19:55:56 +0200

    ready

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const upload = multer();
 
+const corsOptions = {
+  origin: "https://take2eu.com", // Update with your frontend URL
+  optionsSuccessStatus: 200,
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -118,3 +123,72 @@ app.post(
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+// import express from "express";
+// import Stripe from "stripe";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const app = express();
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: "2020-08-27",
+// });
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// // Configure CORS middleware
+// const corsOptions = {
+//   origin: "https://take2eu.com", // Update with your frontend URL
+//   optionsSuccessStatus: 200,
+// };
+
+// app.use(cors(corsOptions));
+
+// // Define your route
+// app.post("/create-checkout-session", async (req, res) => {
+//   const { amount } = req.body;
+//   console.log(req.body); // For debugging
+
+//   try {
+//     if (!amount || typeof amount !== "number") {
+//       throw new Error("Amount must be provided and must be a number.");
+//     }
+
+//     const unitAmount = amount * 100;
+
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       line_items: [
+//         {
+//           price_data: {
+//             currency: "eur",
+//             product_data: {
+//               name: "Your Product Name",
+//             },
+//             unit_amount: unitAmount,
+//           },
+//           quantity: 1,
+//         },
+//       ],
+//       mode: "payment",
+//       success_url: "https://take2eu.com/#/succes", // Replace with your actual URL
+//       cancel_url: "https://take2eu.com/#/cancel", // Replace with your actual URL
+//     });
+
+//     res.json({ id: session.id });
+//   } catch (error) {
+//     console.error("Error creating checkout session:", error);
+//     res.status(500).send({ error: error.message });
+//   }
+// });
+
+// const PORT = process.env.PORT || 4242;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
